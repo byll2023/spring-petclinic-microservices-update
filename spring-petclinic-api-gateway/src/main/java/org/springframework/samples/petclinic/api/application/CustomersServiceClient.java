a68fa9f01f41803f286e1cfe9285d26fade5e048
byll2023 <agbebyll@gmail.com>
2024-01-22 21:31:17 -0500

    correction du bug clients

--- spring-petclinic-api-gateway/src/main/java/org/springframework/samples/petclinic/api/application/CustomersServiceClient.java
+++ spring-petclinic-api-gateway/src/main/java/org/springframework/samples/petclinic/api/application/CustomersServiceClient.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.springframework.samples.petclinic.api.application
+package org.springframework.samples.petclinic.api.application;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.samples.petclinic.api.dto.OwnerDetails;
